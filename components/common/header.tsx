983fcd377903ebcf5e3ade2ca204e42ef5f30213
AreebMughal <areebarshad.m@gmail.com>
2025-07-03 22:46:55 +0500

    update header

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,12 +1,6 @@
-// Copyright Ayush Singh 2021,2022. All Rights Reserved.
-// Project: folio
-// Author contact: https://www.linkedin.com/in/alphaayush/
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-import Image from "next/image";
-import { useState } from "react";
-import Menu from "@/components/common/menu";
+import Image from 'next/image';
+import { useState } from 'react';
+import Menu from '@/components/common/menu';
 
 const Header = () => {
   const [menuVisible, setmenuVisible] = useState(false);
@@ -15,14 +9,9 @@ const Header = () => {
     <header className="w-full fixed top-0 py-8 select-none z-50 bg-gradient-to-b from-gray-900 to-transparent">
       <div className="flex justify-between section-container">
         <a href="#home" className="link">
-          <Image
-            src="/logo.svg"
-            alt="Logo - Ayush Singh"
-            width={22}
-            height={22}
-          />
+          <Image src="/logo.svg" alt="Logo - Areeb Arshad" width={22} height={22} />
         </a>
-        <nav className={`outer-menu ${menuVisible ? "menu-visible" : ""}`}>
+        <nav className={`outer-menu ${menuVisible ? 'menu-visible' : ''}`}>
           <button
             className="hamburger w-6 h-6 flex items-center justify-center link relative"
             onClick={setmenuVisible.bind(null, !menuVisible)}
