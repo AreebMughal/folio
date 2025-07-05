81425ff3f852782212f9ae17cdae807304828209
AreebMughal <areebarshad.m@gmail.com>
2025-07-06 01:57:42 +0500

    create skill name enum

---
+++ enum.ts
@@ -0,0 +1,64 @@
+export enum SKILLS_ENUM {
+  // Backend
+  JAVASCRIPT = 'javascript',
+  TYPESCRIPT = 'typescript',
+  NODEJS = 'nodejs',
+  EXPRESS = 'express',
+  NESTJS = 'nestjs',
+  GRAPHQL = 'graphql',
+  PYTHON = 'python',
+  FLASK = 'flask',
+  SOCKETS = 'sockets',
+
+  // Database
+  MONGODB = 'mongodb',
+  POSTGRESQL = 'postgresql',
+  MYSQL = 'mysql',
+  REDIS = 'redis',
+  FIREBASE = 'firebase',
+
+  // Frontend
+  REACT = 'react',
+  NEXT = 'next',
+  REDUX = 'redux',
+  REACT_QUERY = 'react-query',
+  TAILWIND = 'tailwind',
+  MUI = 'mui',
+  ANTD = 'antd',
+  SVG = 'svg',
+  HTML = 'html',
+  CSS = 'css',
+  SASS = 'sass',
+  ANGULAR = 'angular',
+  GASP = 'gsap',
+
+  // Cloud
+  AWS = 'aws',
+  EC2 = 'aws-ec2',
+  LAMBDA = 'aws-lambda',
+  S3 = 'aws-s3',
+  SNS = 'aws-sns',
+  SES = 'aws-ses',
+  SQS = 'aws-sqs',
+  GCP = 'gcp',
+  DOCKER = 'docker',
+
+  // Integration
+  STRIPE = 'stripe',
+  SENDGRID = 'sendgrid',
+  TWILIO = 'twilio',
+  AWS_LAMBDA = 'aws-lambda',
+  AWS_SNS = 'aws-sns',
+  AWS_S3 = 'aws-s3',
+  AWS_SES = 'aws-ses',
+  AWS_SQS = 'aws-sqs',
+  TRELLO = 'trello',
+
+  // Other
+  GIT = 'git',
+  GITHUB = 'github',
+  POSTMAN = 'postman',
+  JIRA = 'jira',
+  NGINX = 'nginx',
+  CLICKUP = 'clickup'
+}
